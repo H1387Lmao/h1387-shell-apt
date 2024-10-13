89857e81b7c4e9ef4a7761407cf8fc74733e2795
H1387Lmao <h1387.dion@gmail.com>
2024-10-13 08:35:37 +0800

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,5 +1,10 @@
 def echo(args):
-   print(" ".join(args))
+   new=[]
+   for i,v in enumerate(args):
+      if i == 0:
+         continue
+      new.append(v)
+   print(" ".join(new))
 
 
 basic_shell_commands["echo"] = echo
